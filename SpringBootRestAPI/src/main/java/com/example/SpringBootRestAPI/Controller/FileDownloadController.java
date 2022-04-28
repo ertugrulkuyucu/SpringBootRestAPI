d963568a571d0d156645f39473d931a0a80afd89
Ertuğrul Kuyucu <75902272+ertugrulkuyucu@users.noreply.github.com>
2022-04-28 14:46:29 +0300

    3

--- SpringBootRestAPI/src/main/java/com/example/SpringBootRestAPI/Controller/FileDownloadController.java
+++ SpringBootRestAPI/src/main/java/com/example/SpringBootRestAPI/Controller/FileDownloadController.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bson.BasicBSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.mongodb.client.FindIterable;
@@ -14,18 +15,26 @@ import com.mongodb.client.MongoDatabase;
 @RestController
 public class FileDownloadController {
 
+	@Value("${mongoDb}")
+	String mongoDb;	
+	
+	@Value("${dbName}")
+	String dbName;	
+	
+	@Value("${tableName}")
+	String tableName;	
+	
 	@GetMapping("/get")
-	//@RequestMapping(value = "/get", method = RequestMethod.GET)
-	public static BasicBSONObject mongoDbToJson() throws IOException{
+	public BasicBSONObject mongoDbToJson() throws IOException{
+		
 		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
 		mongoLogger.setLevel(Level.SEVERE);
 
-		try (var mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-			MongoDatabase database = mongoClient.getDatabase("Veribel");
-
+		try (var mongoClient = MongoClients.create(mongoDb)) {
+			MongoDatabase database = mongoClient.getDatabase(dbName);
 			BasicBSONObject json = new BasicBSONObject();
 			// Creating a collection object
-			MongoCollection<org.bson.Document> collection = database.getCollection("xrtest");
+			MongoCollection<org.bson.Document> collection = database.getCollection(tableName);
 			// Retrieving the documents
 			FindIterable<org.bson.Document> iterDoc = collection.find();
 			Iterator it = iterDoc.iterator();
